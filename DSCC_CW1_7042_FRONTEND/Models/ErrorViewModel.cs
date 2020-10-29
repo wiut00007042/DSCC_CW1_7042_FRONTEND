86c811b31d46e14a21c70a80ab74621ac71db1a5
wiut00007042 <misokov@students.wiut.uz>
2020-10-29 21:56:43 +0500

    My First Commit

---
+++ DSCC_CW1_7042_FRONTEND/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DSCC_CW1_7042_FRONTEND.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
